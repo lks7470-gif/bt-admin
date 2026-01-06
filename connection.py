e12eda4de0f6e0254dbf85db4c6022ffe6753a88
lks7470-gif <lks7470@gmail.com>
2026-01-06 10:37:12 +0900

    Update connection.py

--- connection.py
+++ connection.py
@@ -4,14 +4,12 @@ from supabase import create_client
 @st.cache_resource
 def get_supabase_client():
     try:
-        # secrets.toml 파일에서 [supabase] 라는 이름의 서랍을 엽니다.
-        # 아래 두 줄이 핵심입니다! 절대 주소를 직접 적지 마세요.
+        # 👇 [핵심] 주소를 직접 쓰지 말고, 이렇게 ["supabase"]["url"] 이라고 적어야 합니다!
         url = st.secrets["supabase"]["https://fkebyokmlhkbxcbyjijb.supabase.co"]
         key = st.secrets["supabase"]["eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZrZWJ5b2ttbGhrYnhjYnlqaWpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY4NjY4MTUsImV4cCI6MjA4MjQ0MjgxNX0.SRvsxwIa6oIUoqlAJBl1lDy1sSM27CZiCYEsDzkIyhc"]
         
         return create_client(url, key)
         
     except Exception as e:
-        st.error("❌ DB 연결 설정 오류 (connection.py)")
-        st.error(f"에러 내용: {e}")
+        st.error(f"❌ DB 연결 실패: {e}")
         return None
